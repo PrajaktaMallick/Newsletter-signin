efa9d1e336a248b38427b2562c991bfc8c579dc1
prajakta mallick <mallickprajakta@gmail.com>
2023-07-17 13:23:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req, res) {
+
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                  FNAME:firstName,
+                  LNAME:lastName
+                }
+            }
+        ]
+    };
+
+
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url: "https://us8.api.mailchimp.com/3.0/lists/2b9fc33a00",
+        
+        method: "POST",
+        headers: {
+            Authorization: "auth 16614aee3aa761a98275ad518c9836a7-us8"
+        },
+        body: jsonData
+    };
+    request(options, function (error, response, body) {
+        if (error) {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        else {
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+            }
+            else {
+                res.sendFile(__dirname+"/failure.html");
+            }
+        }
+
+    });
+});
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log("server is running on port 3000");
+});
+
+//https://us8.admin.mailchimp.com/lists/dashboard/overview?id=865560
+// Api Key --- 16614aee3aa761a98275ad518c9836a7-us8
+//  2b9fc33a00
